c1c9fa53fff346dac30f20d04273341a3ffbee24
ezzep66 <51804994+ezzep66@users.noreply.github.com>
2020-06-23 11:50:25 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const fetch = require("node-fetch")
+
+app.get("/", (req, res) => {
+  let url = "https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?rand=t&n=1";
+
+	fetch(url).then(data => data.json())
+	.then(result => res.send(result))
+})
+
+app.listen(3000, console.log("ok"))
