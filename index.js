4d87cbad1bffe389bc7fd1dcd8bbe628acc6fcef
ezzep66 <51804994+ezzep66@users.noreply.github.com>
2020-06-23 21:35:10 -0300

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const fetch = require("node-fetch")
+const cors = require("cors")
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   let url = "https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?rand=t&n=1";
