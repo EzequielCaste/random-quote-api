586f8839a60f95947100539255239bbe0d2f0c56
ezzep66 <51804994+ezzep66@users.noreply.github.com>
2020-06-24 21:55:59 -0300

    api routes

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const cors = require("cors")
 
 app.use(cors())
 
-app.get("/", (req, res) => {
-  let url = "https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?rand=t&n=1";
+app.get("/:num", (req, res) => {
+  let url = "https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?rand=t&n=" + req.params.num;
 
 	fetch(url)
 		.then(data => data.json())
-		.then(result => res.json(result))
-		
+		.then(result => res.json(result))		
+})
 
-	
+app.get("/", (req, res) => {
+	res.redirect("/1");
 })
 
 app.listen(process.env.PORT || 3000, console.log("ok"))
